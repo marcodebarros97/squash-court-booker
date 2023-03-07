6c9177eb0539dbd6520929ab11f824f2a288a498
Marco De Barros <marcodebarros@Marcos-MacBook-Pro.local>
2023-03-07 19:16:35 +0100

    login and initial setup

--- src/services/login_service.py
+++ src/services/login_service.py
@@ -1,8 +1,29 @@
 # Elements that we will need here
+import logging
+import os
+
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+
 username_input_box_name = 'username'
 password_input_box_name = 'password'
 login_button_class = 'button3 fw'
 
+USERNAME = str(os.getenv('USERNAME'))
+PASSWORD = str(os.getenv('PASSWORD'))
 
-def perform_login(driver):
-    return
+
+def perform_login(driver: WebDriver):
+    username_input_field = driver.find_element(By.NAME, 'username')
+    password_input_field = driver.find_element(By.NAME, 'password')
+
+    if username_input_field is not None and password_input_field is not None:
+        username_input_field.send_keys(USERNAME)
+        password_input_field.send_keys(PASSWORD)
+        driver.find_element(By.XPATH, "//div[@class='form-submit3']//button[@class='button3 fw']").click()
+        if driver.find_element(By.ID, 'calendar_date_title'):
+            print('Login to booking system was successful')
+            logging.info('Login to booking system was successful')
+            return True
+        else:
+            return False
--- src/squash_court_booker.py
+++ src/squash_court_booker.py
@@ -1,10 +1,15 @@
 import os
+import logging
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-BASE_URL = os.getenv('BASE_URL')
+from src.services import login_service
+
+BASE_URL = str(os.getenv('BASE_URL'))
+BOOKING_DATE = str(os.getenv('BOOKING_DATE'))
 
 # Example of URL to use to get to specific date-
 # https://kampongsquash.baanreserveren.nl/reservations/2023-3-7/sport/893
@@ -13,3 +18,13 @@ BASE_URL = os.getenv('BASE_URL')
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get(BASE_URL)
+if driver.find_element(By.ID, 'login-form-container') is not None:
+    print('Correct Page Retrieved, Performing Login')
+    logging.info('Correct Page Retrieved, Performing Login')
+    login_successful = login_service.perform_login(driver)
+    if login_successful:
+        driver.get(f'https://kampongsquash.baanreserveren.nl/reservations/{BOOKING_DATE}/sport/893')
+        booking_system_date = driver.find_element(By.ID, "matrix_date_title").text
+        if BOOKING_DATE in booking_system_date:
+            print(f'Calendar set to day: {booking_system_date}')
+            logging.info(f'Calendar set to day: {booking_system_date}')
