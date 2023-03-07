d457f193784394a95a5d0a176231770e9e0e7400
Marco De Barros <marcodebarros@Marcos-MacBook-Pro.local>
2023-03-07 16:39:30 +0100

    initial code

--- src/services/login_service.py
+++ src/services/login_service.py
@@ -2,3 +2,7 @@
 username_input_box_name = 'username'
 password_input_box_name = 'password'
 login_button_class = 'button3 fw'
+
+
+def perform_login(driver):
+    return
--- src/squash_court_booker.py
+++ src/squash_court_booker.py
@@ -1,11 +1,15 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-# URL To KampongSqaush
-website_url = 'https://kampongsquash.baanreserveren.nl/'
+BASE_URL = os.getenv('BASE_URL')
 
 # Example of URL to use to get to specific date-
 # https://kampongsquash.baanreserveren.nl/reservations/2023-3-7/sport/893
 
 # Take a screenshot of booking and email to me but squash system will also email it
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get(BASE_URL)
