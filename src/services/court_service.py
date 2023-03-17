c73b9be4b6a18b64aaeaa77c565ef893ffcad284
Marco De Barros <marcodebarros@Marcos-MacBook-Pro.local>
2023-03-17 13:48:14 +0100

    code cleanup

--- src/services/court_service.py
+++ src/services/court_service.py
@@ -1,35 +1,34 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 
-# Elements that we will need here
-# Interacting with the Calendar
-# Table rows are structured by time- 30 minute increments so 1 hour is two blocks
 table_row_time_slot_element = 'data-time'
-# Type will be free/taken/blocked so find by free
-time_slot_type_element = 'free'
-# for example squash 1 is court 1
+slot_type = '[type="free"]'
 squash_court_number_title = 'title'
+table_name = 'tbl_matrix'
+tr = 'tr'
+tbody = 'tbody'
 
 
 def map_free_squash_courts(free_time_slots, input_time):
     squash_courts = {}
 
     for time_slot in free_time_slots:
-        free_squash_courts = time_slot.find_elements(By.CSS_SELECTOR, '[type="free"]')
+        free_squash_courts = time_slot.find_elements(By.CSS_SELECTOR, slot_type)
         for squash_court in free_squash_courts:
-            squash_court_number = squash_court.get_attribute("title")
+            squash_court_number = squash_court.get_attribute(squash_court_number_title)
             squash_courts[squash_court_number] = squash_court
 
     return squash_courts
 
 
 def find_all_open_courts(driver: WebDriver, first_time_slot: str):
-    table_id = driver.find_element(By.ID, 'tbl_matrix')
-    table_body = table_id.find_element(By.TAG_NAME, 'tbody')
-    table_rows = table_body.find_elements(By.TAG_NAME, "tr")
+    corresponding_time_slot = f'[data-time="{first_time_slot}"]'
+    table_id = driver.find_element(By.ID, table_name)
+    table_body = table_id.find_element(By.TAG_NAME, tbody)
+    table_rows = table_body.find_elements(By.TAG_NAME, tr)
     first_time_slots = []
 
     for row in table_rows:
-        first_time_slots.extend(row.find_elements(By.CSS_SELECTOR, f'[data-time="{first_time_slot}"]'))
+        first_time_slots.extend(row.find_elements(By.CSS_SELECTOR, corresponding_time_slot))
 
     return map_free_squash_courts(first_time_slots, first_time_slot)
--- src/services/login_service.py
+++ src/services/login_service.py
@@ -1,4 +1,3 @@
-# Elements that we will need here
 import logging
 import os
 
@@ -9,22 +8,23 @@ from selenium.webdriver.support import expected_conditions as EC
 
 username_input_box_name = 'username'
 password_input_box_name = 'password'
-login_button_class = 'button3 fw'
+calendar_title = 'calendar_date_title'
+login_button = "//div[@class='form-submit3']//button[@class='button3 fw']"
 
 USERNAME = str(os.getenv('USERNAME'))
 PASSWORD = str(os.getenv('PASSWORD'))
 
 
 def perform_login(driver: WebDriver):
-    username_input_field = driver.find_element(By.NAME, 'username')
-    password_input_field = driver.find_element(By.NAME, 'password')
+    username_input_field = driver.find_element(By.NAME, username_input_box_name)
+    password_input_field = driver.find_element(By.NAME, password_input_box_name)
 
     if username_input_field is not None and password_input_field is not None:
         username_input_field.send_keys(USERNAME)
         password_input_field.send_keys(PASSWORD)
-        driver.find_element(By.XPATH, "//div[@class='form-submit3']//button[@class='button3 fw']").click()
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'calendar_date_title')))
-        if driver.find_element(By.ID, 'calendar_date_title'):
+        driver.find_element(By.XPATH, login_button).click()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, calendar_title)))
+        if driver.find_element(By.ID, calendar_title):
             print('Login to booking system was successful')
             logging.info('Login to booking system was successful')
             return True
